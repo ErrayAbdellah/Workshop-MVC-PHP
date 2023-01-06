898a313208e0c80389a57683ad1e569743b8d1f3
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-01-06 16:34:01 +0100

    update

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
 
 <?php require 'app/Views/view/head.php' ;
 
-    // $test = null ;
-
-    // if(isset($_GET["Add"])){
-        // $test = 1;
-        require "app/Views/addBluges.php" ;
-
-    // }
-
-    // if($test){
-    //     require "app/Views/addBluges.php" ;
-    // }
+        // require "app/Views/addBluges.php" ;
 ?>
 
-<!-- <input type="submit" value="ADD" name="btn"> -->
-
-<!-- <button> <a href="<?= $_SERVER["PHP_SELF"]?>?Add"> Click</a></button> -->
-
+<section class="card h-100 bg-black">
+    <div>
+        g
+    </div>
+</section>
 
 
 
